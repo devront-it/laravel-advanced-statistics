0f6da3095233de57c46bb57b99bf92a21bed9320
David <david.massanek@devront.com>
2023-08-02 16:37:22 +0200

    query for multiple owners

--- tests/ReadAndWriteStatisticsTest.php
+++ tests/ReadAndWriteStatisticsTest.php
@@ -60,3 +60,46 @@ it('can hit and get statistics with params', function () {
     expect($total_orders)->toBe(9.0);
     expect($total_orders_for_user_for_source_in_germany)->toBe(3.0);
 });
+
+
+it('can hit and get statistics for multiple owners', function () {
+    $user1 = User::find(1);
+    $user2 = User::find(2);
+    $user3 = User::find(3);
+    $statistics = new OrderStatistics();
+    $statistics
+        ->for($user1)
+        ->forCountry('de')
+        ->forSource('source 1')
+        ->hit(3);
+
+    $statistics
+        ->for($user2)
+        ->forCountry('fr')
+        ->forSource('source 1')
+        ->hit(3);
+
+    $statistics
+        ->for($user3)
+        ->forCountry('fr')
+        ->forSource('source 1')
+        ->hit(3);
+
+    $statistics = new OrderStatistics();
+    $total_orders_for_first_and_third_user = $statistics
+        ->for(User::class, [1, 3])
+        ->get();
+    expect($total_orders_for_first_and_third_user)->toBe(6.0);
+
+    $statistics = new OrderStatistics();
+    $total_orders_for_first_and_third_user = $statistics
+        ->for(User::class)
+        ->get();
+    expect($total_orders_for_first_and_third_user)->toBe(9.0);
+
+    $statistics = new OrderStatistics();
+    $total_orders_for_first_and_third_user = $statistics
+        ->for(User::class, [])
+        ->get();
+    expect($total_orders_for_first_and_third_user)->toBe(0);
+});
